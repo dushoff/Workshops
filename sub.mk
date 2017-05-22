ec23b60194afec2c84d0e18507a2fd81b3bed37b
Jonathan Dushoff <dushoff@mcmaster.ca>
2017-05-22 16:13:15 -0400

    Working on submodule initialization

--- sub.mk
+++ sub.mk
@@ -1,7 +1,17 @@
 ms = makestuff
 
-makestuff:
-	git submodule init $@ || git submodule add git@github.com:dushoff/$@.git
+## I think this rule will only be used once per repo.
+## It's here in case I decide to automatically copy it
+$(ms):
+	git submodule add git@github.com:dushoff/$@.git
+
+## It looks like git clone is making the subdirectory, so we need to focus on making the files
+## Don't use a dependency here, as it will lead to re-initialization
+## The recursive make is also mostly useless …:
+$(ms)/%.mk: 
+	$(MAKE) $(ms)
+	git submodule init $(ms) 
+	git submodule update $(ms) 
 
 -include local.mk
 -include $(ms)/os.mk
